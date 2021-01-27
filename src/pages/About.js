ae0412c6d089536e367f75ec209af0e6283c0ba8
danailkostov <dankostov359@gmail.com>
2021-01-27 21:27:29 +0200

    about-us v2

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,207 +4,273 @@ import {
   Card,
   CardContent,
   CardMedia,
+  CardActions,
+  Button,
+  CardActionArea,
+  Avatar,
+  Link,
 } from "@material-ui/core";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import InstagramIcon from "@material-ui/icons/Instagram";
 import React from "react";
+
 // backgroundColor: "#e2dff0",
 //     color: "#802634",
 const About = () => {
   return (
-    <Grid
-      container
-      style={{
-        paddingTop: "70px",
-        paddingLeft: "60px",
-        backgroundColor: "#e2dff0",
-        color: "#802634",
-        paddingBottom: "50px",
-      }}
-    >
-      <Grid item xs={2}></Grid>
-
-      <Grid item xs={3} container>
-        <Grid item xs={12}>
-          <img
-            style={{ height: "550px", width: "100%" }}
-            src="https://images.unsplash.com/photo-1511376979163-f804dff7ad7b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mzl8fHdlbGNvbWUlMjBtZWV0aW5nfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-            alt=""
-          />
+    <Grid container direction="column" spacing={2}>
+      <Grid
+        item
+        container
+        style={{
+          height: "30vh",
+          backgroundColor: "#802634",
+        }}
+      >
+        <Grid item xs={5}></Grid>
+        <Grid item xs={2} style={{ alignSelf: "center", color: "#e2dff0" }}>
+          <Typography align="center" variant="h3">
+            ABOUT US
+          </Typography>
         </Grid>
+        <Grid item xs={5}></Grid>
       </Grid>
-
-      <Grid item xs={5} container>
+      <Grid
+        item
+        container
+        style={{
+          height: "60vh",
+          backgroundColor: "#e2dff0",
+        }}
+      >
+        <Grid item xs={3}></Grid>
         <Grid
           item
-          xs={12}
-          style={{
-            paddingLeft: "30px",
-          }}
+          xs={6}
+          container
+          spacing={10}
+          style={{ backgroundColor: "#802634", margin: "100px 0px" }}
         >
-          <Card
-            style={{
-              backgroundColor: "#e2dff0",
-              color: "#802634",
-              boxShadow: "none",
-            }}
-          >
-            <CardContent>
-              <Typography gutterBottom>WELCOME TO COMPANY NAME</Typography>
-              <Typography variant="h5" component="h2">
-                We are Furniture Services
-              </Typography>
-              <br />
-              <Typography variant="body2" component="p">
-                A small river named Duden flows by their place and supplies it
-                with the necessary regelialia. It is a paradisematic country, in
-                which roasted parts of sentences fly into your mouth.
-              </Typography>
-              <br />
-              <Typography variant="body2" component="p">
-                Even the all-powerful Pointing has no control about the blind
-                texts it is an almost unorthographic life One day however a
-                small line of blind text by the name of Lorem Ipsum decided to
-                leave for the far World of Grammar.
-              </Typography>
-              <br />
-              <Typography variant="body2" component="p">
-                A small river named Duden flows by their place and supplies it
-                with the necessary regelialia. It is a paradisematic country, in
-                which roasted parts of sentences fly into your mouth.
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={12} container>
+          <Grid item xs={7}>
+            <Card
+              style={{
+                backgroundColor: "#802634",
+                color: "#e2dff0",
+                boxShadow: "none",
+              }}
+            >
+              <CardContent>
+                <Typography
+                  style={{ borderBottom: "1px solid #e2dff0", width: "4vw" }}
+                >
+                  About Us
+                </Typography>
+                <br />
+                <Typography variant="h5">
+                  We Believe that Interior beauty Lasts Long
+                </Typography>
+                <br />
+                <Typography variant="body2">
+                  inappropriate behavior is often laughed off as “boys will be
+                  boys,” women face higher conduct standards especially in the
+                  workplace. That’s why it’s crucial that as women.
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="medium" style={{ backgroundColor: "#e2dff0" }}>
+                  Learn More
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
           <Grid
             item
+            xs={5}
             style={{
-              paddingLeft: "30px",
-              width: "250px",
-              height: "80px",
-              backgroundColor: "#802634",
-              color: "#e2dff0",
-              paddingTop: "15px",
-              borderRadius: '5px',
-              marginLeft: '5px'
+              alignSelf: "center",
+              position: "relative",
+              left: "5vw",
+              padding: "40px 0px",
             }}
           >
-            <Typography>20</Typography>
-            <Typography>YEARS OF EXPERIENCE</Typography>
+            <Card style={{ border: "1px solid white" }}>
+              <CardActionArea>
+                <CardMedia
+                  style={{ height: "25vh" }}
+                  image="https://images.unsplash.com/photo-1611689225620-3e70248bc0f0?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                />
+              </CardActionArea>
+            </Card>
           </Grid>
         </Grid>
+        <Grid item xs={3}></Grid>
       </Grid>
-
-      <Grid item xs={12} container spacing={2} style={{ paddingTop: "100px" }}>
-        <Grid item xs={12}>
+      <Grid
+        item
+        container
+        style={{
+          backgroundColor: "#802634",
+          paddingBottom: "130px",
+        }}
+      >
+        <Grid item xs={4}></Grid>
+        <Grid item xs={4} style={{ color: "#e2dff0", paddingTop: "15px" }}>
           <Typography align="center">ABOUT US</Typography>
-        </Grid>
-        <Grid item xs={12}>
+          <br />
           <Typography align="center" variant="h3">
             OUR STAFF
           </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <Typography align="center" variant="subtitle2">
+          <br />
+          <Typography align="center" variant="body1">
             Far far away, behind the word mountains, far from the countries
             Vokalia and Consonantia
           </Typography>
+          <br />
         </Grid>
-        <Grid item xs={12} container spacing={2} style={{ paddingTop: "50px" }}>
-          <Grid item xs={2}></Grid>
-          <Grid item xs={2}>
+        <Grid item xs={4}></Grid>
+        <Grid item xs={3}></Grid>
+        <Grid item xs={6} container justify="space-between">
+          <Grid item xs={3}>
             <Card
               style={{
-                textAlign: "center",
-                backgroundColor: "#e2dff0",
-                width: "170px",
+                textAlign: "-webkit-center",
+                color: "#e2dff0",
+                backgroundColor: "#802634",
+                border: "1px solid white",
+                paddingTop: "15px",
               }}
             >
+              <Avatar
+                style={{ width: "150px", height: "150px" }}
+                src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+              />
               <CardContent>
-                <CardMedia
-                  style={{
-                    height: "140px",
-                    borderRadius: "50%",
-                  }}
-                  image="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-                />
+                <Typography gutterBottom variant="h5" component="h2">
+                  Lloyd Wilson
+                </Typography>
+                <Typography variant="body2" component="p">
+                  CEO, FOUNDER
+                </Typography>
                 <br />
-                <Typography gutterBottom>Lloyd Wilson</Typography>
-
-                <Typography>CEO, FOUNDER</Typography>
+                <Link
+                  underline="none"
+                  href="https://www.youtube.com/"
+                  style={{ color: "white", paddingRight: "5px" }}
+                >
+                  <FacebookIcon />
+                </Link>
+                <Link
+                  underline="none"
+                  href="https://www.youtube.com/"
+                  style={{ color: "white", paddingRight: "5px" }}
+                >
+                  <TwitterIcon />
+                </Link>
+                <Link
+                  underline="none"
+                  href="https://www.youtube.com/"
+                  style={{ color: "white", paddingRight: "5px" }}
+                >
+                  <InstagramIcon />
+                </Link>
               </CardContent>
             </Card>
           </Grid>
-          <Grid item xs={2}>
+          <Grid item xs={3}>
             <Card
               style={{
-                textAlign: "center",
-                backgroundColor: "#e2dff0",
-                width: "170px",
+                textAlign: "-webkit-center",
+                color: "#e2dff0",
+                backgroundColor: "#802634",
+                border: "1px solid white",
+                paddingTop: "15px",
               }}
             >
+              <Avatar
+                style={{ width: "150px", height: "150px" }}
+                src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+              />
               <CardContent>
-                <CardMedia
-                  style={{
-                    height: "140px",
-                    borderRadius: "50%",
-                  }}
-                  image="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-                />
+                <Typography gutterBottom variant="h5" component="h2">
+                  Lloyd Wilson
+                </Typography>
+                <Typography variant="body2" component="p">
+                  CEO, FOUNDER
+                </Typography>
                 <br />
-                <Typography gutterBottom>Lloyd Wilson</Typography>
-
-                <Typography>CEO, FOUNDER</Typography>
+                <Link
+                  underline="none"
+                  href="https://www.youtube.com/"
+                  style={{ color: "white", paddingRight: "5px" }}
+                >
+                  <FacebookIcon />
+                </Link>
+                <Link
+                  underline="none"
+                  href="https://www.youtube.com/"
+                  style={{ color: "white", paddingRight: "5px" }}
+                >
+                  <TwitterIcon />
+                </Link>
+                <Link
+                  underline="none"
+                  href="https://www.youtube.com/"
+                  style={{ color: "white", paddingRight: "5px" }}
+                >
+                  <InstagramIcon />
+                </Link>
               </CardContent>
             </Card>
           </Grid>
-          <Grid item xs={2}>
+          <Grid item xs={3}>
             <Card
               style={{
-                textAlign: "center",
-                backgroundColor: "#e2dff0",
-                width: "170px",
+                textAlign: "-webkit-center",
+                color: "#e2dff0",
+                backgroundColor: "#802634",
+                border: "1px solid white",
+                paddingTop: "15px",
               }}
             >
+              <Avatar
+                style={{ width: "150px", height: "150px" }}
+                src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+              />
               <CardContent>
-                <CardMedia
-                  style={{
-                    height: "140px",
-                    borderRadius: "50%",
-                  }}
-                  image="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-                />
+                <Typography gutterBottom variant="h5" component="h2">
+                  Lloyd Wilson
+                </Typography>
+                <Typography variant="body2" component="p">
+                  CEO, FOUNDER
+                </Typography>
                 <br />
-                <Typography gutterBottom>Lloyd Wilson</Typography>
-
-                <Typography>CEO, FOUNDER</Typography>
+                <Link
+                  underline="none"
+                  href="https://www.youtube.com/"
+                  style={{ color: "white", paddingRight: "5px" }}
+                >
+                  <FacebookIcon />
+                </Link>
+                <Link
+                  underline="none"
+                  href="https://www.youtube.com/"
+                  style={{ color: "white", paddingRight: "5px" }}
+                >
+                  <TwitterIcon />
+                </Link>
+                <Link
+                  underline="none"
+                  href="https://www.youtube.com/"
+                  style={{ color: "white", paddingRight: "5px" }}
+                >
+                  <InstagramIcon />
+                </Link>
               </CardContent>
             </Card>
           </Grid>
-          <Grid item xs={2}>
-            <Card
-              style={{
-                textAlign: "center",
-                backgroundColor: "#e2dff0",
-                width: "170px",
-              }}
-            >
-              <CardContent>
-                <CardMedia
-                  style={{
-                    height: "140px",
-                    borderRadius: "50%",
-                  }}
-                  image="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-                />
-                <br />
-                <Typography gutterBottom>Lloyd Wilson</Typography>
-
-                <Typography>CEO, FOUNDER</Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item xs={2}></Grid>
         </Grid>
+        <Grid item xs={3}></Grid>
       </Grid>
     </Grid>
   );
